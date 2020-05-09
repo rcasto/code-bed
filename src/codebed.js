0c7f148396da622aaefe9f3b84ce61241b8460c3
Richie Casto <rcasto92@gmail.com>
2020-05-09 01:20:06 -0700

    core support for lazy loading on visibility

--- src/codebed.js
+++ src/codebed.js
@@ -7,17 +7,25 @@ import {
 } from './codepenUtil';
 import {
   copyAttributes,
-  createTemplateFromString
+  createTemplateFromString,
+  createIntersectionObserver
 } from './domUtil';
 
 // https://blog.codepen.io/documentation/embedded-pens/#the-embed-code-0
 const embedTemplateContent = `
-  <div class="${codepenEmbedClass}">
-    No CodePen connected
-  </div>
+  <div class="code-bed-no-pen">No CodePen connected</div>
 `;
 
 const templateContent = `
+  <style>
+    .code-bed-embed-container {
+      display: flex;
+      flex-direction: column;
+    }
+    .code-bed-embed-container .code-bed-no-pen {
+      margin: auto;
+    }
+  </style>
   <div>
     <div class="${codepenEmbedContainerClass}">
       ${embedTemplateContent}
@@ -37,24 +45,56 @@ export default class CodeBed extends HTMLElement {
   constructor() {
     super();
 
+    this.intersectionObserver =
+      createIntersectionObserver(intersections => this.onIntersectionUpdates(intersections));
+
     this.codepenEmbedContainerElem = null;
     this.codepenEmbedElem = null;
+  }
+  get codePenHeight() {
+    // Note: The default fallback 300, is the default height of
+    // CodePen embeds not specifying a height
+    return this.getAttribute('data-height') || '300';
+  }
+  get codePenSlugHash() {
+    return this.getAttribute('data-slug-hash') || '';
+  }
+  onIntersectionUpdates(intersections) {
+    const lastIntersectionRecord = intersections[intersections.length - 1];
+
+    if (lastIntersectionRecord?.isIntersecting) {
+        this.reloadCodePenEmbed();
+    }
+  }
+  resetEmbedContent() {
+    if (!this.codepenEmbedContainerElem) {
+      return;
+    }
+
+    this.codepenEmbedContainerElem.textContent = '';
+    this.codepenEmbedContainerElem.style.height = `${this.codePenHeight}px`;
+  
+    const embedTemplateClone = embedTemplate.content.cloneNode(true);
+    this.codepenEmbedContainerElem.appendChild(embedTemplateClone);
+    
+    this.codepenEmbedElem = this.codepenEmbedContainerElem.querySelector('div');
+
+    // If there is a code pen hash, add a special class
+    // including this hash to allow individual triggering
+    // or loading of it when it comes into view
+    if (this.codePenSlugHash) {
+      this.codepenEmbedElem.classList.add(`${codepenEmbedClass}-${this.codePenSlugHash}`);
+    }
+
+    copyAttributes(this, this.codepenEmbedElem, codepenEmbedAttributes);
   }
   reloadCodePenEmbed() {
     if (!this.codepenEmbedContainerElem) {
       return;
     }
 
-    this.codepenEmbedContainerElem.textContent = '';
-  
-    const embedTemplateClone = embedTemplate.content.cloneNode(true);
-    this.codepenEmbedContainerElem.appendChild(embedTemplateClone);
-    
-    this.codepenEmbedElem = this.codepenEmbedContainerElem.querySelector(`.${codepenEmbedClass}`);
-    copyAttributes(this, this.codepenEmbedElem, codepenEmbedAttributes);
-
     loadCodePenEmbedScript()
-      .then(triggerCodePenEmbedReload)
+      .then(() => triggerCodePenEmbedReload(this.codePenSlugHash))
       // Would probably want to handle this better later
       .catch(err => {
         this.codepenEmbedElem.textContent = 'An error occurred while loading the CodePen';
@@ -71,10 +111,33 @@ export default class CodeBed extends HTMLElement {
     this.appendChild(templateClone);
 
     this.codepenEmbedContainerElem = this.querySelector(`.${codepenEmbedContainerClass}`);
+    this.resetEmbedContent();
+
+    /*
+      If the intersection observer API is supported then we will
+      let that trigger the loading of the CodePen embed, when the
+      CodePen embed container elem is brought into view.
+
+      If the intersection observer API is not supported, then we will
+      immediately load the CodePen.
+    */
+    if (this.intersectionObserver) {
+      this.intersectionObserver.observe(this.codepenEmbedContainerElem);
+    } else {
+      this.reloadCodePenEmbed();
+    }
+  }
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.resetEmbedContent();
     this.reloadCodePenEmbed();
   }
-  attributeChangedCallback() {
-    this.reloadCodePenEmbed();
+  disconnectedCallback() {
+    if (this.intersectionObserver) {
+      this.intersectionObserver.unobserve(this.codepenEmbedContainerElem);
+    }
+
+    this.codepenEmbedContainerElem = null;
+    this.codepenEmbedElem = null;
   }
 }
 
